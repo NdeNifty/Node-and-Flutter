7ab83b82ccc30598c2baf916da3bc57ceece1069
NdeNifty <Ndekong@gmail.com>
2023-01-26 23:46:40 +0100

    few changes to log file names

--- 30 days of Node/day 4/nodelogfile.js
+++ 30 days of Node/day 4/nodelogfile.js
@@ -1,13 +1,13 @@
 //stroring console output to a logfile
 var ws = require('fs');
 const { Console } = require('console');
-const output = ws.createWriteStream('./stdout.log');
-const errOutput = ws.createWriteStream('./stderr.log');
+const output = ws.createWriteStream('./filename.log');
+const errOutput = ws.createWriteStream('./anotherfilename.log');
 // custom simple print
 const print = new Console(output, errOutput);
 //Now we can print to the log files
-const roll = 839147;
-print.log('roll: %d', roll);
+const roll = 'We are on the 4th day of a 30 day journey!!!';
+print.log(roll);
 print.log('This will be stored in a file');	
 
 //and we can still print to the console

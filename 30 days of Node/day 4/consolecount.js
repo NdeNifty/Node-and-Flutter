f8229f940cc53b88937e5309ad35aa188bb98f34
NdeNifty <Ndekong@gmail.com>
2023-01-26 22:49:36 +0100

    Console count

---
+++ 30 days of Node/day 4/consolecount.js
@@ -0,0 +1,12 @@
+//the console count is uded to count distinct instances of a console output
+console.count('dog');
+console.count('cat');
+console.count('dog');
+console.count('cat');
+console.count('cat');
+console.count('chicken');
+console.count('pig');
+console.count('pig');
+console.count('dog');
+console.count('chicken');
+	

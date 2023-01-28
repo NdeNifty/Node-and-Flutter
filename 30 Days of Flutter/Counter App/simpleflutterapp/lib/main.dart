6bd13d2a73290fd152f41b16e78c62f58bdcfbd0
NdeNifty <Ndekong@gmail.com>
2023-01-28 19:04:11 +0100

    Playing around with slutter stateless widgets

--- 30 Days of Flutter/Counter App/simpleflutterapp/lib/main.dart
+++ 30 Days of Flutter/Counter App/simpleflutterapp/lib/main.dart
@@ -1,70 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyFirstApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'My First Flutter App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('The title of the app'),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            DecoratedBox(
+              decoration: BoxDecoration(color: Colors.amber),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text('Hello World'),
+              ),
+            ),
+            DecoratedBox(
+              decoration: BoxDecoration(color: Colors.purple),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text('Hello World'),
+              ),
+            )
+          ],
+        )),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: const Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              ),
-            ],
-          ),
-        ),
-        /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        const Text('41'),
-      ],
-    ),
-  );
 }

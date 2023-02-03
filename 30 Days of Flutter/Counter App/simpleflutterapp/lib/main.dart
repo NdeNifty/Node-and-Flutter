3baff2619d484a71be852a017a058104743820cb
NdeNifty <Ndekong@gmail.com>
2023-02-03 22:35:28 +0100

    added color to text

--- 30 Days of Flutter/Counter App/simpleflutterapp/lib/main.dart
+++ 30 Days of Flutter/Counter App/simpleflutterapp/lib/main.dart
@@ -23,14 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -120,12 +113,16 @@ class _MyHomePageState extends State<MyHomePage> {
                       ? 'You lost!'
                       : 'Your move'),
               OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      tiles = List.filled(9, 0);
-                    });
-                  },
-                  child: Text('Restart'))
+                onPressed: () {
+                  setState(() {
+                    tiles = List.filled(9, 0);
+                  });
+                },
+                child: Text('Restart',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                    )),
+              ),
             ],
           )
         ],
